0c85a98b269b8a0473684af4dd05d0d5ca2b8857
Jon Lee <agentlee927@gmail.com>
2017-09-01 15:23:00 -0400

    score fixed

--- Tutorial/Roids/Assets/Scripts/ScoreUI.cs
+++ Tutorial/Roids/Assets/Scripts/ScoreUI.cs
@@ -8,16 +8,14 @@ public class ScoreUI : MonoBehaviour
 	GUIText scoreText;
 
 	// Use this for initialization
-	void Start () 
-	{
+	void Start () {
 		GameObject g = GameObject.Find ("GlobalObject");
 		globalObj = g.GetComponent< Global >();
 		scoreText = gameObject.GetComponent<GUIText>();
 	}
-	
+
 	// Update is called once per frame
-	void Update () 
-	{
-		scoreText.text = globalObj.score.ToString();
+	void Update () {
+		scoreText.text = "Score: " + globalObj.score.ToString();
 	}
 }
